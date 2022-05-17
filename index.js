db8780190ef1951eb80b168bdf6929bf78205ae8
Harshil Gupta <harshilgupta0099@gmail.com>
2022-05-18 01:47:15 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const randomize_array=document.getElementById("random_array_btn");
 const sort_btn=document.getElementById("sort_btn");
-
+let bars_container=document.getElementById("bar_container");
 let minRange=1;
-let maxRange=100;
-let noOfBars=10;
+let maxRange=30;
+let noOfBars=40;
+let heightFactor=6.5;
 
 let unsorted_array=new Array(noOfBars);
 
@@ -31,3 +32,51 @@ function renderBars(array){
         bars_container.appendChild(bar);
     }
 }
+
+randomize_array.addEventListener("click",function(){
+    createRandomArray();
+    bars_container.innerHTML="";
+    renderBars(unsorted_array);
+});
+
+function sleep(ms){
+    return new Promise((resolve)=>setTimeout(resolve,ms));
+}
+
+async function bubbleSort(array){
+    let bars =document.getElementsByClassName("bar");
+    for(let i=0;i<array.length;i++){
+        for(let j=0;j<array.length-i-1;j++){
+            if(array[j]>array[j+1]){
+
+                for(let k=0;k<bars.length;k++){
+                    if(k!==j&&k!==(j+1))
+                    {
+                        bars[k].style.backgroundColor="white";
+                    }    
+                }
+
+                let temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+
+                bars[j].style.height=array[j]*heightFactor+"px";
+                bars[j].style.backgroundColor="lightgreen";
+                // bars[j].innerText=array[j];
+
+                bars[j+1].style.height=array[j+1]*heightFactor+"px";
+                bars[j+1].style.backgroundColor="lightgreen";
+                // bars[j+1].innerText=array[j+1];
+                await sleep(30);
+            }
+        }
+        await sleep(30);
+    }
+    return array;
+}
+
+
+sort_btn.addEventListener("click",function(){
+    let sorted_array=bubbleSort(unsorted_array);
+    console.log(sorted_array);
+});
